48b335278b9f35a83b4e4f32b139473b016619a5
SafiKhan934 <safiurrehman810760@gmail.com>
2026-03-29 15:33:34 +0500

    Update WhatsappButton.js

--- src/Components/WhatsappButton/WhatsappButton.js
+++ src/Components/WhatsappButton/WhatsappButton.js
@@ -2,7 +2,7 @@ import styles from "./WhatsappButton.module.css"
 function WhatsappButton({ watch }) {
   const phoneNumber = "+923707189797"
   const handleWhatsAppClick = () => {
-    const message = `Hello! I'm interested in this watch:\nName: ${watch.name}\nOriginal-Price: ${watch.price?.original}\ndiscount-price : {watch.price?.discount}\nCheck image: ${watch.image?.url}`
+    const message = `Hello! I'm interested in this watch:\nName: ${watch.name}\nOriginal-Price: ${watch.price?.original}\ndiscount-price : ${watch.price?.discount}\nCheck image: ${watch.image?.url}`
     const encodedMessage = encodeURIComponent(message)
     const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`
     window.open(whatsappLink, "_blank")
