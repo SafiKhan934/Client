73fd333a7e92c23897a7e2b0f3ca130eff3b016d
Saad Muhammad Bin Ramzan <184134144+saadkhan86@users.noreply.github.com>
2026-03-31 12:47:00 +0500

    Remove unused useNavigate import from Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,7 @@
 import React from "react"
 import styles from "./Navbar.module.css"
-import { Link, useNavigate } from "react-router-dom"
+import { Link} from "react-router-dom"
 function Navbar() {
-  const navigate = useNavigate()
   return (
     <div>
       <div className={styles.navbar}>
